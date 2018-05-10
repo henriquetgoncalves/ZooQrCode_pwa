1232c9f143aa12bb310c68d6560b1ea841281c13
henriquetgoncalves <henrique.develop@hotmail.com>
2018-05-10 15:25:21 -0300

    fixed id and validation text of image

--- private/scripts/app_adm.js
+++ private/scripts/app_adm.js
@@ -158,7 +158,7 @@ function ValidaInfos(animal){
     }
     else if (document.getElementById('fileselect').files.length <= 0){
         bFlag=false;
-        cCampo = "Selecione uma imagem para o animal.";        
+        cCampo = "imagem para o animal.";        
     }
 
     snackbar_show("Preencha o campo "+cCampo,10000);
@@ -189,7 +189,7 @@ function saveAnimal() {
     
     if( ValidaInfos(animal)){
         //add animal
-        if(typeof id !== "undefined" && id=="") {
+        if(typeof id === "undefined" || id=="") {
             snackbar_show("Incluindo o animal " + animal.nome + "...", 10000);
 
             // Get a key for a new Animal.    
